0813362c47ff43754ae5136d69e00c80da090338
Tyler Evans <tkevans4@gmail.com>
2018-08-10 16:10:22 +0000

    added iteration practice

---
+++ iteration_python/iterate_practice.py
@@ -0,0 +1,37 @@
+# my_arr = [1,2,3]
+
+# for num in my_arr:
+#     print(num)
+    
+    
+
+    
+
+book1 = {
+    "title": "mobydick",
+    "author": "Herman Melville"
+}
+
+book2 = {
+    "title": "gone girl",
+    "author": "Gillian Flynn"
+}
+    
+book3 = {
+    "title": "catcher in the rye",
+    "author": "J. D. Salinger"
+}
+
+book4 = {
+    "title": "tom sawyer",
+    "author":"mark twain"
+}
+
+books = [book1, book2, book3, book4]
+
+for book in books:
+    # print(book["title"])
+    # print(book["author"])
+    
+    print("*********************************************")
+    print("book title: {} \nbook author: {}\n".format(book["title"], book["author"]))

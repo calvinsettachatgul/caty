61ad3a2ccd64f2809cf7001c0551664571762211
Tyler Evans <tkevans4@gmail.com>
2018-08-11 15:36:39 +0000

    added more routes

--- flask_app/hello_world/app.py
+++ flask_app/hello_world/app.py
@@ -6,5 +6,23 @@ app = Flask(__name__)
 def index():
     return "hello world"
     
+@app.route('/about')
+def about(): # new view function about
+    return "about page"
+    
+@app.route('/helloworld/<name>')
+def helloworld(name): # new view function about
+    print(name)
+    return "hello world"
+
+@app.route('/welcome/<username>')
+def welcome(username):
+    print(username)
+    return "welcome"
+
+@app.route('/contact')
+def contact(): # new view function contact
+    return "my email is ...@gmail.com"
+
 if __name__ == "__main__":
     app.run("0.0.0.0", port=8080, debug=True)

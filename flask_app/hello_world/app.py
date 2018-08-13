5a0f5fbc75c552ea9d2bf29d8f0e1d8c5979dd78
EC2 Default User <ec2-user@ip-172-31-90-31.ec2.internal>
2018-08-13 16:09:06 +0000

    add render_template

--- flask_app/hello_world/app.py
+++ flask_app/hello_world/app.py
@@ -1,24 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "hello world"
+    
+    # return a render_template instead of a hard coded string
+    data_from_database = {"user1": "user1firstname"}
+    return render_template('index.html', data=data_from_database)
     
 @app.route('/about')
 def about(): # new view function about
-    return "about page"
+
+    # return a render_template instead of hard coded about string
+    about_data = {"owner1": "owner_firstName"}
+    return render_template('about.html', owner=about_data)
     
 @app.route('/helloworld/<name>')
 def helloworld(name): # new view function about
     print(name)
-    return "hello world"
+    return_message = "Hello World {}".format(name)
+    return return_message 
 
 @app.route('/welcome/<username>')
+
 def welcome(username):
     print(username)
-    return "welcome"
+    return "Welcome Dear {}".format(username)
 
 @app.route('/contact')
 def contact(): # new view function contact
